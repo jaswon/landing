2ef7893fb617c8b844d9188c1cf1498c08a46a61
jaswon5791 <jasonwon628@gmail.com>
2017-07-28 12:12:02 -0400

    added commands to terminal

--- terminal.js
+++ terminal.js
@@ -1,21 +1,104 @@
+const h2c = String.fromCodePoint; // unicode hex -> character
+
+function balloon (text) {
+  let lines = (function ( str, width ) { // https://j11y.io/snippets/wordwrap-for-javascript/
+    if (!str) return str;
+    var regex = String.raw`.{1,${width}}(\s|$)|.{${width}}|.+$`;
+    return str.match( RegExp(regex, 'g') );
+  })(text, 40).map(s => s.trim())
+  let maxLength = lines.reduce((prev, cur) => {
+    return (prev>cur.length)?prev:cur.length
+  },0)
+  let delims = {
+    f: ["/","\\"],
+    m: ["|","|"],
+    l: ["\\","/"]
+  }
+  if (lines.length == 1) {
+    return [
+      " " + new Array(maxLength+3).join("_"),
+      "< " + lines[0] + " >",
+      " " + new Array(maxLength+3).join("-")
+    ].join("\n")
+  } else {
+    let balloon = [" " + new Array(maxLength+3).join("_")]
+    for (var i = 0 ; i < lines.length ; ++i) {
+      let delim = delims[i==0?"f":(i==lines.length-1?"l":"m")]
+      balloon.push(delim[0]+" "+lines[i]+(new Array(maxLength - lines[i].length + 1)).join(" ")+" "+delim[1])
+    }
+    return balloon.concat(" " + new Array(maxLength+3).join("-")).join("\n")
+  }
+}
+
+function cowsay (text) {
+  return String.raw`${balloon(text)}
+    \   ^__^
+     \  (oo)\_______
+        (__)\       )\/\
+            ||----w |
+            ||     ||
+  `
+}
+
 (function() {
-  var terminal = document.querySelector("#terminal")
-  var lines = document.querySelector('#lines')
-  var curLine = document.querySelector("#input")
+  const trigger = "'"
+
+  const terminal = document.querySelector("#terminal")
+  const lines = document.querySelector('#lines')
+  const curLine = document.querySelector("#input")
 
   var terminalOpen = false;
 
-  function print(content) {
-    var line = document.createElement("li")
-    line.innerHTML = "- "+content
-    lines.appendChild(line)
-    lines.scrollTop = lines.scrollHeight
+  const aliases = (function(rev) {
+    let ret = {}
+    for (var key in rev)
+      rev[key].forEach(function(v) {
+        ret[v] = key
+      })
+    return ret
+  })({
+    'random': ['rand'],
+    'hello': ['hi','hey'],
+    'cowsay': ['moo']
+  })
+
+  const convertAlias = command => (command in aliases)?aliases[command]:command
+
+  const commands = {
+    'random': max => max?`${h2c(0x1F3B2)} ${Math.floor(Math.random()*max)}`:'gimme a numbah',
+    'hello': () => `hi!${h2c(0x1F44B)}`,
+    'echo': (...args) => args.join(" "),
+    'cowsay': (...args) => cowsay(args.join(" ")),
+    'cls': () => {
+      while (lines.lastChild) lines.removeChild(lines.lastChild);
+      return ""
+    }
+  }
+
+  function print(content, res) {
+    if (content != "")
+      content.split('\n').forEach(function(l) {
+        var line = document.createElement("li")
+        line.innerHTML = (res?"- ":"> ")+l
+        lines.appendChild(line)
+        terminal.scrollTop = terminal.scrollHeight
+      })
+  }
+
+  function evaluate(tokens) {
+    return commands[tokens[0]].apply(null, tokens.slice(1))
   }
 
   function onTermKey (e) {
     if (e.keyCode == 13) { // enter
+      let tokens = curLine.innerHTML.trim().split(" ")
+      tokens[0] = convertAlias(tokens[0])
       print(curLine.innerHTML);
+      if (tokens[0] in commands) {
+        print(evaluate(tokens), true)
+      }
       curLine.innerHTML = "";
+      terminal.scrollTop = terminal.scrollHeight
     } else if (e.keyCode == 8) { // backspace
       if (e.ctrlKey) {
         curLine.innerHTML = curLine.innerHTML
@@ -27,13 +110,13 @@
         curLine.innerHTML = curLine.innerHTML.slice(0,-1)
       }
     } else if (e.key.length == 1) {
-      curLine.innerHTML += e.key
+      if (!(e.key == trigger && (e.ctrlKey || e.metaKey) )) curLine.innerHTML += e.key
     }
     e.preventDefault()
   }
 
   document.querySelector("html").onkeydown = function(e) {
-    if (e.key == "`" && e.altKey) {
+    if (e.key == trigger && (e.ctrlKey || e.metaKey) ) {
       terminalOpen = !terminalOpen
       terminal.className = ""
       terminal.className = terminalOpen?"show":"hide"
